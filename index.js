8472f1241c77e1e04966c457d036095bc831c255
Colin Tseng <klt.aus@yahoo.com>
2021-03-09 22:12:21 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name: "James",
+    streetAddress: "Bond Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee [key] = value;
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee [key] = value;
+        return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee [key] = value;
+    delete newEmployee.key;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    employee [key] = value;
+    delete employee.key;
+    return employee;
+}
